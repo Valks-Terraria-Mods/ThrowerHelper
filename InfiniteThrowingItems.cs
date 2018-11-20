8b226e430643aeaa47a0a7b4e24d831714cafffb
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-20 00:50:14 -0600

    commit

---
+++ InfiniteThrowingItems.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ThrowersHelper
+{
+    class InfiniteThrowingItems : GlobalItem
+    {
+        public override bool ConsumeItem(Item item, Player player) {
+            if (item.thrown) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ThrowersHelper.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace ThrowersHelper
+{
+	class ThrowersHelper : Mod
+	{
+		public ThrowersHelper()
+		{
+		}
+	}
+}
